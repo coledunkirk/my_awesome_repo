af575207e15ce60d07cdf4fb9b3bb91d2496bfaf
coledunkirk <81267459+coledunkirk@users.noreply.github.com>
2021-03-23 20:10:57 -0400

    Add files via upload

---
+++ launchschool/Sindy.rb
@@ -0,0 +1,12 @@
+puts "what is your name"
+name = gets.chomp
+puts "hello " + name
+10.times do
+  puts name
+end
+puts "What is your last name?"
+last_name = gets.chomp
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Your name is: "
+puts first_name + " " + last_name
